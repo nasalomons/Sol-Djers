c716c35a988571e6aef2a65ad9dce64ad9c56c14
Noah Salomons <nsalomon@terpmail.umd.edu>
2019-04-20 13:33:48 -0400

    camera movement and inital main scene

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    public GameObject player;
+
+    // Update is called once per frame
+    void Update() {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 15, player.transform.position.z - 9);
+    }
+}
