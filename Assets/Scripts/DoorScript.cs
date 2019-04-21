0fab400d46c1a3d707aa6e97b931e4d06d3c8378
Noah Salomons <nsalomon@terpmail.umd.edu>
2019-04-21 16:40:54 -0400

    door opens when near which is neat

---
+++ Assets/Scripts/DoorScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour {
+
+    public GameObject[] characters;
+
+    private Animator animator;
+
+    void Start() {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update() {
+        foreach (GameObject character in characters) {
+            Vector3 distance = character.transform.position - transform.position;
+            if (distance.magnitude <= 7) {
+                animator.SetBool("character_nearby", true);
+                return;
+            }
+        }
+        animator.SetBool("character_nearby", false);
+    }
+}
