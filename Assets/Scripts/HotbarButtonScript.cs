7dc7ba1b42b9521d12f7e5c96f29473bcd92f9d9
Noah Salomons <nsalomon@terpmail.umd.edu>
2019-05-13 19:57:35 -0400

    ability descriptions

--- Assets/Scripts/HotbarButtonScript.cs
+++ Assets/Scripts/HotbarButtonScript.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class HotbarButtonScript : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+    private GameObject description;
+
+    void Start() {
+        description = transform.GetChild(2).gameObject;
+    }
 
     public void OnPointerEnter(PointerEventData data) {
         SelectManager.Instance.SetOverButton(true);
+        description.SetActive(true);
     }
 
     public void OnPointerExit(PointerEventData data) {
         SelectManager.Instance.SetOverButton(false);
+        description.SetActive(false);
     }
 }
