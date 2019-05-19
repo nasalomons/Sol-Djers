7c03969ae5ab090c9fb1575124e8d03edc10c025
Noah Salomons <nsalomon@terpmail.umd.edu>
2019-05-19 10:19:39 -0400

    quit button

--- Assets/Scripts/MenuNavigation/MainMenu.cs
+++ Assets/Scripts/MenuNavigation/MainMenu.cs
@@ -5,13 +5,19 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour {
-    public Button button;
+    public Button start;
+    public Button quit;
 
     private void Start() {
-        button.onClick.AddListener(Begin);
+        start.onClick.AddListener(Begin);
+        quit.onClick.AddListener(Quit);
     }
 
     private void Begin() {
         SceneManager.LoadScene("MainScene");
+    }
+
+    private void Quit() {
+        Application.Quit();
     }
 }
